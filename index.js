e30daa3bce0280acfb5aa51ea7b8aaa1a029543d
Zachary Flower <zach@ninjaninja.net>
2017-08-18 21:14:51 -0600

    Actual confirm code

---
+++ index.js
@@ -0,0 +1,35 @@
+const { dialog } = require('electron');
+
+let confirmQuit = false;
+
+exports.decorateConfig = (config) => {
+  if (config.confirmQuit) {
+    confirmQuit = !!config.confirmQuit;
+  }
+
+  return config;
+};
+
+exports.onApp = (app) => {
+  let quitConfirmed = false;
+
+  app.on('before-quit', (event) => {
+    if (!!confirmQuit && !quitConfirmed && app.getWindows().size ) {
+      event.preventDefault();
+
+      dialog.showMessageBox({
+        type: 'question',
+        buttons: ['OK', 'Cancel'],
+        defaultId: 0,
+        title: 'Quit HyperTerm?',
+        message: 'Quit HyperTerm?',
+        detail: 'All sessions will be closed.'
+      }, (index) => {
+        if (index === 0) {
+          quitConfirmed = true;
+          app.quit();
+        }
+      });
+    }
+  });
+}
